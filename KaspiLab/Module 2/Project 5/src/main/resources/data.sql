407bb8501e3551cc22953f5d3cd950ed74c024a8
aselraim <a_raimbaeva@kbtu.kz>
2026-01-09 18:17:56 +0500

    Committed Project 5 of Module 2

--- KaspiLab/Module 2/Project 5/src/main/resources/data.sql
+++ KaspiLab/Module 2/Project 5/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO products VALUES
+INSERT INTO products (name, price) VALUES
     ('iPhone 17', 899990);
-INSERT INTO products VALUES
+INSERT INTO products (name, price) VALUES
     ('Samsung Galaxy S26', 799990);
-INSERT INTO products VALUES
+INSERT INTO products (name, price) VALUES
     ('Xiaomi Note 17 Pro', 389990);
-INSERT INTO products VALUES
+INSERT INTO products (name, price) VALUES
     ('Lenovo Yoga S940', 589990);
-INSERT INTO products VALUES
+INSERT INTO products (name, price) VALUES
     ('MacBook Pro', 1290990);
